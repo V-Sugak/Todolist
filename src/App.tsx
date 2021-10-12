2f72b33dd7cc8d2577e01b2f279a5f8aa23821d0
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-12 23:09:45 +0300

    01

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import {TasksType, Todolist} from "./Todolist";
+
+
+
+function App() {
+
+    let tasks1: Array<TasksType> = [
+        {id: 1, title: 'CSS&HTML', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
+
+    let tasks2:Array<TasksType> = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: 'Gentlemen of fortune', isDone: true},
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title='What to learn' tasks={tasks1}/>
+            <Todolist title='Movies' tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
