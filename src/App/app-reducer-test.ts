497c053cb968a570a5b593b198fc35ac78e19978
V-Sugak <vicka.sugak2010@yandex.ru>
2022-11-16 23:03:17 +0300

    Add tests for appReducer

---
+++ src/App/app-reducer-test.ts
@@ -0,0 +1,30 @@
+import {appReducer, RequestStatusType, setAppErrorAC, setAppStatusAC} from "./app-reducer";
+
+type InitialStateType = {
+    status: RequestStatusType,
+    error: null | string,
+    isInitialized: boolean
+}
+let startState: InitialStateType;
+
+beforeEach(() => {
+    startState = {
+        error: null,
+        status: 'idle',
+        isInitialized: false,
+    }
+})
+
+test('correct error message should be set', () => {
+
+    const endState = appReducer(startState, setAppErrorAC({error: 'some error'}))
+
+    expect(endState.error).toBe('some error');
+})
+
+test('correct status should be set', () => {
+
+    const endState = appReducer(startState, setAppStatusAC({status: 'loading'}))
+
+    expect(endState.status).toBe('loading');
+})
